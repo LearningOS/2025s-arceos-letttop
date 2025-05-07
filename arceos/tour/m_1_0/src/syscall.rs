fb2d4c07d3d48327e657671c7986bbe51d006681
lett <letterr2022@gmail.com>
2025-05-08 01:33:49 +0800

    m1

--- arceos/tour/m_1_0/src/syscall.rs
+++ arceos/tour/m_1_0/src/syscall.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
-use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
 use axerrno::LinuxError;
+use axhal::arch::TrapFrame;
+use axhal::trap::{register_trap_handler, PAGE_FAULT, SYSCALL};
 
 const SYS_EXIT: usize = 93;
 
@@ -13,11 +13,26 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         SYS_EXIT => {
             ax_println!("[SYS_EXIT]: process is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         _ => {
             ax_println!("Unimplemented syscall: {}", syscall_num);
             -LinuxError::ENOSYS.code() as _
         }
     };
     ret
+}
+
+use axhal::mem::VirtAddr;
+use axhal::paging::MappingFlags;
+use axtask::TaskExtRef;
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(vaddr: VirtAddr, mut access_flags: MappingFlags, is_user: bool) -> bool {
+    if is_user {
+        access_flags |= MappingFlags::USER;
+    }
+    axtask::current()
+        .task_ext()
+        .aspace
+        .lock()
+        .handle_page_fault(vaddr, access_flags)
 }
