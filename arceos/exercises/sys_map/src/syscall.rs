99af308067c7815a1c839e4e4847918503cc6629
lett <letterr2022@gmail.com>
2025-05-14 02:00:03 +0800

    m2: mmap

--- arceos/exercises/sys_map/src/syscall.rs
+++ arceos/exercises/sys_map/src/syscall.rs
@@ -1,13 +1,17 @@
 #![allow(dead_code)]
 
-use core::ffi::{c_void, c_char, c_int};
-use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
+use crate::std::vec;
+use arceos_posix_api::{self as api, get_file_like};
 use axerrno::LinuxError;
+use axhal::arch::TrapFrame;
+use axhal::mem::VirtAddr;
+use axhal::paging::MappingFlags;
+use axhal::trap::{register_trap_handler, PAGE_FAULT, SYSCALL};
 use axtask::current;
 use axtask::TaskExtRef;
-use axhal::paging::MappingFlags;
-use arceos_posix_api as api;
+use core::ffi::{c_char, c_int, c_void};
+use memory_addr::{MemoryAddr, VirtAddrRange};
+use std::io::BufRead;
 
 const SYS_IOCTL: usize = 29;
 const SYS_OPENAT: usize = 56;
@@ -100,9 +104,14 @@ bitflags::bitflags! {
 fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
     ax_println!("handle_syscall [{}] ...", syscall_num);
     let ret = match syscall_num {
-         SYS_IOCTL => sys_ioctl(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _) as _,
+        SYS_IOCTL => sys_ioctl(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _) as _,
         SYS_SET_TID_ADDRESS => sys_set_tid_address(tf.arg0() as _),
-        SYS_OPENAT => sys_openat(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _, tf.arg3() as _),
+        SYS_OPENAT => sys_openat(
+            tf.arg0() as _,
+            tf.arg1() as _,
+            tf.arg2() as _,
+            tf.arg3() as _,
+        ),
         SYS_CLOSE => sys_close(tf.arg0() as _),
         SYS_READ => sys_read(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
         SYS_WRITE => sys_write(tf.arg0() as _, tf.arg1() as _, tf.arg2() as _),
@@ -110,11 +119,11 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         SYS_EXIT_GROUP => {
             ax_println!("[SYS_EXIT_GROUP]: system is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         SYS_EXIT => {
             ax_println!("[SYS_EXIT]: system is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         SYS_MMAP => sys_mmap(
             tf.arg0() as _,
             tf.arg1() as _,
@@ -131,16 +140,92 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
     ret
 }
 
-#[allow(unused_variables)]
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(vaddr: VirtAddr, mut access_flags: MappingFlags, is_user: bool) -> bool {
+    ax_println!(
+        "handle_page_fault: vaddr={:?}, flags={:?}, is_user={}",
+        vaddr,
+        access_flags,
+        is_user
+    );
+    if is_user {
+        access_flags |= MappingFlags::USER;
+    }
+    axtask::current()
+        .task_ext()
+        .aspace
+        .lock()
+        .handle_page_fault(vaddr, access_flags)
+}
+
 fn sys_mmap(
     addr: *mut usize,
     length: usize,
     prot: i32,
     flags: i32,
     fd: i32,
-    _offset: isize,
+    offset: isize,
 ) -> isize {
-    unimplemented!("no sys_mmap!");
+    let length = length.align_up_4k();
+
+    // find current task
+    let cur_task = axtask::current();
+    let mut aspace = cur_task.task_ext().aspace.lock();
+
+    // find free space for mapping
+    let Some(vaddr) = aspace.find_free_area(
+        unsafe { VirtAddr::from_mut_ptr_of(addr) },
+        length,
+        VirtAddrRange::new(aspace.base(), aspace.end()),
+    ) else {
+        warn!("No free space for mapping");
+        return -LinuxError::ENOMEM.code() as isize;
+    };
+
+    // Protection Flags
+    // 0x001: read
+    // 0x002: write
+    // 0x004: execute
+    let prot_flags = MmapProt::from_bits_truncate(prot);
+    let mut map_flags = MappingFlags::from(prot_flags);
+    // allocate memory for mapping
+    if let Err(e) = aspace.map_alloc(vaddr, length, map_flags, true) {
+        warn!("Failed to allocate memory for mapping: {:?}", e);
+        return -LinuxError::ENOMEM.code() as isize;
+    }
+
+    // Mapping Flags
+    // 0x001: shared
+    // 0x002: private, copy on write
+    // 0x020: ANONYMOUS
+    // let mmap_flags = MmapFlags::from_bits_truncate(flags);
+
+    // read file
+    let file = match get_file_like(fd) {
+        Ok(file) => file,
+        Err(e) => {
+            ax_println!("sys_mmap: get_file_like failed: {:?}", e);
+            return -LinuxError::EBADF.code() as isize;
+        }
+    };
+    let mut buf = vec![0; length];
+    file.read(&mut buf)
+        .map_err(|e| {
+            ax_println!("sys_mmap: read file failed: {:?}", e);
+            LinuxError::EIO
+        })
+        .unwrap();
+
+    // map file to address space
+    aspace
+        .write(vaddr, &buf)
+        .map_err(|e| {
+            ax_println!("sys_mmap: write to address space failed: {:?}", e);
+            LinuxError::ENOMEM
+        })
+        .unwrap();
+
+    vaddr.as_usize() as isize
 }
 
 fn sys_openat(dfd: c_int, fname: *const c_char, flags: c_int, mode: api::ctypes::mode_t) -> isize {
